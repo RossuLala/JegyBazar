f7cca2b31dd3c53fbcac797975ac40a5d774ff35
RossuLala <lala@intralog.hu>
2018-10-23 09:32:52 +0200

    collapse - responsiv menü meggjelenítése https://valor-software.com/ngx-bootstrap/old/1.9.3/index-bs4.html#/collapse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jegybazar';
+  public title = 'jegybazar';
+  public isCollapsed:boolean = true;
 }
