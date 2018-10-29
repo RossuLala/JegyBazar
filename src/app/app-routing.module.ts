02dc2c1d99a679759df9c58aa7ff67f6867fdc12
RossuLala <lala@intralog.hu>
2018-10-29 19:24:16 +0100

    Objektumok létrehozása és összekapcsolása ++

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,8 @@ import {LoginComponent} from './login/login.component';
 import {RegistrationComponent} from './registration/registration.component';
 import {EventListComponent} from './event-list/event-list.component';
 import {EventDetailComponent} from './event-detail/event-detail.component';
+import {ProfileComponent} from './profile/profile.component';
+import {ProfileEditComponent} from './profile-edit/profile-edit.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
@@ -24,6 +26,11 @@ const routes: Routes = [
   { path: 'about', component: AbautComponent},
   { path: 'login', component: LoginComponent},
   { path: 'registry', component: RegistrationComponent},
+  { path: 'user',
+    children: [
+      {path: '', component: ProfileComponent},
+      {path: 'edit', component: ProfileEditComponent},
+    ]},
   { path: '', redirectTo: '/home', pathMatch: 'full'}, // csak akkor teljesen üres
   { path: '**', component: PageNotFoundComponent}
   // fontos a sorend, mert sorrendben hatja végre
@@ -43,6 +50,8 @@ export class AppRoutingModule {
     AbautComponent,
     LoginComponent,
     RegistrationComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    ProfileComponent,
+    ProfileEditComponent
   ];
 }
